2314d778e2591e0cfe86183bc3f15a76d041aa41
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-04-03 11:04:42 -0400

    added HTML routes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 //create array variables that will hold data
 
 //set routes for getting data
@@ -5,5 +7,20 @@
 //set routes for posting data
 
 //set routes for displaying html
+function displayHTML(app) {
+    app.get('/tables', function(req, res) {
+        res.sendFile(path.join(__dirname + 'tables.html'));
+    });
 
+    app.get('/reserve', function(req, res) {
+        res.sendFile(path.join(__dirname + 'reserve.html'));
+    });
+
+    app.use(function(req, res) {
+        res.sendFile(path.join(__dirname + 'tables.html'));
+    });
+}
 //ust jQuery to run AJAZ calls to get & post data
+
+//module exports
+module.exports = displayHTML();
