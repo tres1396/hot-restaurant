a08d544eabd56795afd84ac7b5424ef099af2f16
Brandon King <brandonscottking@gmail.com>
2021-04-03 10:44:06 -0400

    added index.js with comments

---
+++ index.js
@@ -0,0 +1,9 @@
+//create array variables that will hold data
+
+//set routes for getting data
+
+//set routes for posting data
+
+//set routes for displaying html
+
+//ust jQuery to run AJAZ calls to get & post data
